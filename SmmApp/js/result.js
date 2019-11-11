4948fb97b0cf6d6ff5e4e0bf0ef11bae294b55f9
robin2005 <57048685@qq.com>
2019-11-11 14:57:33 +0800

    PsychologicalTest

--- SmmApp/js/result.js
+++ SmmApp/js/result.js
@@ -35,3 +35,5 @@ var result = {
         content:"Abraham Lincoln - who announced that the last Thursday of November as the Federal Thanksgiving Day.<br /><br />You’re overbearing with strong possession. In real life, you’re a cold outside and hot inside person.It’s lucky to be with you, because you’re too gentle to spoil your lover.",
     }
 };
+
+var sgaredata = { sharetitle: 'Happy Thanksgiving', sharecontent: 'Happy Thanksgiving', sharelinkurl: 'Happy Thanksgiving', shareimg: 'Happy Thanksgiving' };
