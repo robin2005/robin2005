ad4665e0e2abf8a519b7321a9aae369490270bab
robin2005 <57048685@qq.com>
2019-11-12 10:32:48 +0800

    PsychologicalTest

--- SmmApp/js/viewport.js
+++ SmmApp/js/viewport.js
@@ -60,34 +60,7 @@ function getQueryVariable(variable) {
 }
 
 function openApp() {
-    let _downLoadUrl = '';
-    let _schema = '';
-    let _protocal = '';
-    if (global.getIOSorAndroid() === 'ios') {
-        _downLoadUrl = "https://apps.apple.com/cn/app/senior-meet-me-dating-50/id1465679728";//ios下载地址
-        _schema = '?open';
-        _protocal = 'smm';
-    } else if (global.getIOSorAndroid() === 'android') {
-        _downLoadUrl = 'http://fusion.qq.com/cgi-bin/qzapps/unified_jump?actionFlag=0&appid=42342315&appinstall=0&params=pname%3Dcom.xituchina.umbrella%26versioncode%3D96%26channelid%3D%26actionflag%3D0&from=mqq';
-        _schema = '?open';
-        _protocal = 'smm';
-    }
-    openAppFun.loadSchema({
-        // 通过NN打开某个链接
-        schema: _schema,
-        //schema头协议，实际情况填写
-        protocal: _protocal,
-        //发起唤醒请求后，会等待loadWaiting时间，超时则跳转到failUrl，默认3000ms
-        loadWaiting: "1000",
-        //唤起失败时的跳转链接，默认跳转到下载页
-        failUrl: _downLoadUrl,
-        // apk信息,请根据实际情况填写
-        apkInfo: {
-            PKG: "com.xituchina.umbrella",
-            CATEGORY: "android.intent.category.DEFAULT",
-            ACTION: "android.intent.action.VIEW"
-        }
-    });
+    openAppFun('smm://',"https://apps.apple.com/cn/app/senior-meet-me-dating-50/id1465679728");
 }
 
 
