16ac9b9a6e7642caa7f4cf03826d1cacab563f76
robin2005 <57048685@qq.com>
2019-11-13 10:12:33 +0800

    PsychologicalTest

--- SmmApp/js/viewport.js
+++ SmmApp/js/viewport.js
@@ -95,9 +95,9 @@ function goDownLoad() {
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
     var isIos = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
     if (isAndroid) {
-        openAppFun('smm://', "https://apps.apple.com/us/app/seniormeetme-date-50-singles/id1465679728");
+        openAppFun('smm://', "https://itunes.apple.com/us/app/id1465679728?mt=8");
     } else if (isIos) {
-        openAppFun('smm://', "https://apps.apple.com/us/app/seniormeetme-date-50-singles/id1465679728");
+        openAppFun('smm://', "https://itunes.apple.com/us/app/id1465679728?mt=8");
     } else {
         window.location.href = "https://www.seniormeetme.com/";
     }
